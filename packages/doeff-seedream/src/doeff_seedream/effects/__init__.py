f21450d2e51594cfea31d9996f7b7681ef12d6b9
proboscis <nameissoap@gmail.com>
2026-02-11 16:59:52 +0900

    Add Seedream domain effects and handlers

---
+++ packages/doeff-seedream/src/doeff_seedream/effects/__init__.py
@@ -0,0 +1,9 @@
+"""Domain effects for doeff-seedream."""
+
+from .generate import SeedreamGenerate
+from .structured import SeedreamStructuredOutput
+
+__all__ = [
+    "SeedreamGenerate",
+    "SeedreamStructuredOutput",
+]
