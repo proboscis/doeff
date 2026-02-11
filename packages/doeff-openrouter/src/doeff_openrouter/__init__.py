10d279859a2d832af4784db1359ec3553cb036e0
proboscis <nameissoap@gmail.com>
2026-02-11 16:19:26 +0900

    Add OpenRouter domain effects and handlers

--- packages/doeff-openrouter/src/doeff_openrouter/__init__.py
+++ packages/doeff-openrouter/src/doeff_openrouter/__init__.py
@@ -8,15 +8,22 @@ from .client import (
     get_total_cost,
     reset_cost_tracking,
 )
+from .effects import RouterChat, RouterStreamingChat, RouterStructuredOutput
+from .handlers import mock_handlers, production_handlers
 from .structured_llm import structured_llm
 
 __all__ = [
     "OpenRouterClient",
     "OpenRouterResponseError",
+    "RouterChat",
+    "RouterStreamingChat",
+    "RouterStructuredOutput",
     "chat_completion",
     "get_model_cost",
     "get_openrouter_client",
     "get_total_cost",
+    "mock_handlers",
+    "production_handlers",
     "reset_cost_tracking",
     "structured_llm",
 ]
